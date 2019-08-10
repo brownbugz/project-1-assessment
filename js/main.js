c10a704b372ef524240fd6559ed3ef93d13d830a
sonniakemmer <brownbugz@gmail.com>
2019-08-09 21:47:23 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,56 @@
+//const
+
+//variables
+var inputBox = document.getElementById('inputSpace');
+var answersBox = document.getElementById('answers');
+var answers = 0;
+
+//cached
+
+//listeners
+
+
+//functions
+init();
+
+// start the page with 1 in the input box
+function init() {
+    console.log('init starts');
+    inputBox.value = 1;
+    showResults();
+    console.log('end of init');
+}
+
+function showResults() {
+    answersBox.innerHTML = answers;
+}
+
+function addNum() {
+    console.log('start addNum')
+    answers = answers + parseInt(inputBox.value);
+    console.log(answers);
+    console.log(inputBox.value);
+    showResults();
+    console.log('this is the final answer');
+}
+
+function deductNum() {
+    answers = answers - parseInt(inputBox.value);
+    showResults();
+}
+
+function addButton() {
+    addNum();
+    showResults();
+}
+
+function minusButton() {
+    deductNum();
+    showResults();
+}
+
+function negativeRed() {
+    if (answers < 0) {
+        answersBox.style.color = "red";
+    }
+}
